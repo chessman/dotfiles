105440bd1809c69ac814330880d508723fe304de
Eugene Apollonsky <e.apollonsky@gmail.com>
2017-03-25 01:21:45 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,447 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Init use-package
+
+(require 'package)
+(setq
+ use-package-always-ensure t
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                    ("org" . "http://orgmode.org/elpa/")
+                    ("melpa" . "http://melpa.org/packages/")
+                    ("melpa-stable" . "http://stable.melpa.org/packages/")))
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Striping & Defaults
+
+(setq inhibit-startup-screen t)
+
+(setq frame-title-format '("" "%b @ Emacs " emacs-version))
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(blink-cursor-mode 0)
+(fset `yes-or-no-p `y-or-n-p)
+
+(setq-default indent-tabs-mode nil)
+
+(set-frame-font "Source Code Pro")
+
+;(setq-default browse-url-browser-function 'eww-browse-url)
+(setq-default browse-url-browser-function 'browse-url-chrome)
+
+(column-number-mode)
+
+(setq-default fill-column 100)
+(auto-fill-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Backup
+
+(setq delete-old-versions t
+  backup-directory-alist `(("." . "~/.saves"))
+  backup-by-copying t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Themes
+
+(use-package molokai-theme
+  :config
+  (load-theme 'molokai t))
+
+;(use-package solarized-theme
+;  :init
+;  (progn
+;    (setq solarized-distinct-fringe-background nil))
+;  :config
+;  (load-theme 'solarized-dark t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Modeline
+
+(use-package smart-mode-line
+  :config
+  (setq sml/no-confirm-load-theme t)
+  (sml/setup))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Evil
+
+(use-package evil
+  :config
+  (evil-mode 1)
+  (define-key evil-normal-state-map (kbd "M-.") nil)
+  (define-key evil-normal-state-map (kbd "<XF86Tools>") 'evil-insert)
+  (define-key evil-insert-state-map (kbd "<XF86Tools>") 'evil-force-normal-state))
+
+(use-package evil-leader
+  :config
+  (progn
+    (evil-leader/set-leader "SPC")
+    (global-evil-leader-mode)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helm
+
+; show only header
+(defun helm-hide-minibuffer-maybe ()
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face (let ((bg-color (face-background 'default nil)))
+                              `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+(use-package helm
+  :diminish helm-mode
+  :init
+  (progn
+    (require 'helm-config)
+    (helm-mode))
+  :bind ("M-x" . helm-M-x)
+  :config
+  (progn
+  (setq helm-echo-input-in-header-line t
+        helm-display-header-line nil
+        helm-always-two-windows t
+        helm-M-x-fuzzy-match t)
+  (add-hook 'helm-minibuffer-set-up-hook 'helm-hide-minibuffer-maybe)
+  (evil-leader/set-key
+   "b" 'helm-mini
+   "ff" 'helm-find-files)))
+
+(defun helm-do-ag-this-dir ()
+ (interactive)
+ (helm-do-ag (projectile-project-root)))
+
+(use-package helm-ag
+ :config (evil-leader/set-key
+   "/" 'helm-do-ag-this-dir))
+
+(use-package helm-swoop
+  :config
+  (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search))
+
+; eshell-helm hacks
+; https://github.com/emacs-helm/helm/wiki/Eshell
+(add-hook 'eshell-mode-hook
+          (lambda ()
+            (eshell-cmpl-initialize)
+            (define-key eshell-mode-map [remap eshell-pcomplete] 'helm-esh-pcomplete)
+            (define-key eshell-mode-map (kbd "M-p") 'helm-eshell-history)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Projectile & recentf
+
+(use-package projectile
+  :diminish projectile-mode
+  :config
+  (projectile-mode)
+  (setq-default projectile-project-root-files-functions
+    '(projectile-root-local
+      projectile-root-top-down
+      projectile-root-bottom-up
+      projectile-root-top-down-recurring))
+  (add-to-list 'projectile-globally-ignored-directories ".ensime_cache")
+  (add-to-list 'projectile-globally-ignored-files ".ensime"))
+
+(use-package helm-projectile
+ :config (evil-leader/set-key
+   "pf" 'helm-projectile-find-file
+   "pp" 'helm-projectile-switch-project))
+
+(use-package recentf
+  :config
+  (recentf-mode t)
+  (add-to-list 'recentf-exclude "\\.ensime"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Octave
+
+(use-package octave
+  :mode ("\\.m\\'" . octave-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Scala
+
+(defun scala-settings ()
+  (setq ensime-sem-high-faces
+	'((var . scala-font-lock:var-face)
+	  (val . (:inherit font-lock-constant-face :slant italic))
+	  (varField . scala-font-lock:var-face)
+	  (valField . (:inherit font-lock-constant-face :slant italic))
+	  (functionCall . font-lock-function-name-face)
+	  (operator . font-lock-keyword-face)
+	  (param . (:slant italic))
+	  (class . font-lock-type-face)
+	  (trait .  (:inherit font-lock-type-face :slant italic))
+	  (object . font-lock-constant-face)
+	  (package . font-lock-preprocessor-face)
+	  (implicitConversion . nil) ;fixed
+	  (implicitParams . nil) ;fixed
+	  (deprecated . (:strike-through "dark gray")))
+
+	ensime-implicit-gutter-icons nil
+
+	ensime-auto-generate-config t
+
+	ensime-company-case-sensitive t
+
+	ensime-use-helm t
+
+	ensime-startup-notification nil
+	ensime-startup-snapshot-notification nil
+
+	ensime-server-logback "/home/ea/tmp/logback.xml"
+
+	scala-indent:use-javadoc-style t
+	scala-indent:default-run-on-strategy scala-indent:reluctant-strategy
+	scala-indent:align-forms nil
+	scala-indent:align-parameters nil
+
+	;; layers/+lang/scala/config.el
+	scala-auto-insert-asterisk-in-comments t
+
+	flycheck-scalastyle-jar "~/bin/scalastyle_2.11-0.6.0.jar"
+	flycheck-scalastylerc "scalastyle-config.xml")
+
+  (ensime-mode))
+
+(use-package ensime
+  :pin melpa
+  :config
+  (progn
+    (evil-leader/set-key
+      "rt" 'ensime-import-type-at-point
+      "ht" 'ensime-print-type-at-point
+      "gg" 'ensime-edit-definition
+      "cc" 'ensime-sbt-do-compile)
+    (add-hook 'scala-mode-hook 'scala-settings)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Go
+
+(defun my-go-mode-hook ()
+  ; Call Gofmt before saving                                                    
+  ;(add-hook 'before-save-hook 'gofmt-before-save)
+  ; Godef jump key binding                                                      
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark))
+
+(use-package go-mode
+  :config
+  (add-hook 'go-mode-hook 'my-go-mode-hook))
+
+(use-package company-go
+  :config
+  (add-to-list 'company-backends 'company-go))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Javascript
+
+(use-package js2-mode
+  :mode (("\\.js$" . js2-mode))
+  :config (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2))))
+
+(use-package js2-refactor)
+
+(use-package tern
+  :diminish " T"
+  :commands (tern-mode)
+  :init (progn
+          (add-hook 'js2-mode-hook 'tern-mode)))
+
+(use-package company-tern
+            :config (progn
+                      (add-to-list 'company-backends 'company-tern)))
+
+(use-package jade
+  :config (add-hook 'js2-mode-hook #'jade-interaction-mode))
+
+(use-package web-mode
+  :mode ("\\.tsx\\'" "\\.jsx\\'")
+  :config
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (or (string-equal "tsx" (file-name-extension buffer-file-name))
+                        (string-equal "jsx" (file-name-extension buffer-file-name)))
+                (tide-setup)))))
+
+(use-package tide
+  :config
+  (add-hook 'typescript-mode-hook #'tide-setup))
+
+(use-package json-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Magit
+
+(use-package magit
+  :config (evil-leader/set-key
+    "g s" 'magit-status))
+
+(use-package evil-magit)
+
+(setq-default ediff-window-setup-function 'ediff-setup-windows-plain)
+(setq-default ediff-prefer-iconified-control-frame t)
+
+(use-package git-timemachine
+  :config
+  (evil-make-overriding-map git-timemachine-mode-map 'normal)
+  ;; force update evil keymaps after git-timemachine-mode loaded
+  (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Windows
+
+(evil-leader/set-key
+  "wh" 'evil-window-left
+  "wl" 'evil-window-right
+  "wj" 'evil-window-down
+  "wk" 'evil-window-up)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Init file
+
+(defun find-user-init-file ()
+  (interactive)
+  (find-file-existing user-init-file))
+
+(defun reload-user-init-file ()
+  (interactive)
+  (load-file user-init-file))
+
+(evil-leader/set-key
+  "fed" 'find-user-init-file
+  "feR" 'reload-user-init-file)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Smartparens
+
+(defmacro def-pairs (pairs)
+  `(progn
+     ,@(loop for (key . val) in pairs
+          collect
+            `(defun ,(read (concat
+                            "wrap-with-"
+                            (prin1-to-string key)
+                            "s"))
+                 (&optional arg)
+               (interactive "p")
+               (sp-wrap-with-pair ,val)))))
+
+(def-pairs ((paren        . "(")
+            (bracket      . "[")
+            (brace        . "{")
+            (single-quote . "'")
+            (double-quote . "\"")
+            (back-quote   . "`")))
+
+(use-package smartparens
+  :config
+  (require 'smartparens-scala)
+  (add-to-list 'sp-sexp-suffix (list 'js2-mode 'regexp "")) ;like smartparens-scala
+  (add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode)
+  (add-hook 'scala-mode-hook (lambda ()
+            (smartparens-strict-mode)))
+  (add-hook 'js2-mode-hook 'smartparens-strict-mode)
+  (bind-keys
+   :map smartparens-mode-map
+   ("M-]" . sp-unwrap-sexp)
+   ("M-[" . sp-backward-unwrap-sexp)
+   ("C-<right>" . sp-slurp-hybrid-sexp)
+   ("M-<right>" . sp-backward-barf-sexp)
+   ("C-<left>"  . sp-backward-slurp-sexp)
+   ("M-<left>"  . sp-forward-barf-sexp)
+   ("C-k (" . wrap-with-parens)
+   ("C-k [" . wrap-with-brackets)
+   ("C-k {" . wrap-with-braces)
+   ("C-k '" . wrap-with-single-quotes)
+   ("C-k \"" . wrap-with-double-quotes)
+   ("C-k t" . sp-transpose-hybrid-sexp)))
+
+(use-package evil-smartparens
+  :config
+  (add-hook 'smartparens-enabled-hook #'evil-smartparens-mode))
+
+; always show mathing paren
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Company
+
+(use-package company
+  :config
+  ; http://emacs.stackexchange.com/questions/10837/how-to-make-company-mode-be-case-sensitive-on-plain-text
+  (setq company-dabbrev-downcase nil)
+  (global-company-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Flycheck
+
+(use-package flycheck
+  :config
+  (global-flycheck-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Restclient
+
+(use-package restclient)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Multitran
+
+(use-package multitran)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org
+
+(setq-default org-startup-indented t)
+(setq-default org-src-fontify-natively t)
+
+(setq org-preview-latex-default-process 'dvisvgm)
+
+(use-package org
+  :ensure org-plus-contrib
+  :config
+  (require 'ox)
+  (require 'org-drill))
+
+(use-package org-evil)
+
+;; this package maps too much (J, for instance)
+;(use-package evil-org)
+
+(use-package org-pomodoro)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "31b2145c933e41fbbda48b15278cdcce3779db7e92ca434ad3044b3392ad6ae3" default)))
+ '(package-selected-packages
+   (quote
+    (company-go go-mode json-mode web-mode use-package tide smart-mode-line restclient php-mode org-pomodoro org-evil multitran monokai-theme molokai-theme js2-refactor jade helm-swoop helm-projectile helm-ag git-timemachine evil-smartparens evil-magit evil-leader ensime company-tern))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ vimrc
@@ -0,0 +1,373 @@
+
+set mouse=a
+
+"буфер иксов
+if has('unnamedplus')
+    set clipboard=unnamedplus,autoselect
+else
+    set clipboard=unnamed
+endif
+
+set completeopt=menu
+
+set cindent shiftwidth=4
+
+set wrap
+set linebreak
+set tw=100
+set expandtab
+set sw=4
+
+set modeline
+
+set hidden
+
+set laststatus=2
+
+set hlsearch            " Highlight
+set incsearch		" Incremental search
+
+set showcmd		" Show (partial) command in status line.
+set showmatch		" Show matching brackets.
+
+"virtualedit in VISUAL BLOCK mode
+set virtualedit=block
+
+if v:version >= 703
+    set undofile
+if has('nvim')
+    set undodir=~/.undodir_nvim
+else
+    set undodir=~/.undodir
+endif
+
+    set colorcolumn=+1
+endif
+
+set langmap=йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ;qwertyuiop[]asdfghjkl\;\'zxcvbnm\\,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>
+
+"------------------------------------------------------------------------
+" Detection
+"------------------------------------------------------------------------
+
+"fish
+if &shell =~# 'fish$'
+    set shell=sh
+endif
+
+"neovim
+if has('nvim')
+    let s:editor_root=expand("~/.config/nvim/")
+else
+    let s:editor_root=expand("~/.vim")
+endif
+
+"------------------------------------------------------------------------
+" Vundle
+"------------------------------------------------------------------------
+set nocompatible
+filetype off
+let s:vundle_path = s:editor_root . '/bundle/Vundle.vim'
+let &rtp = &rtp . ',' . s:vundle_path
+call vundle#begin(s:editor_root . '/bundle')
+
+Plugin 'VundleVim/Vundle.vim'
+
+Plugin 'Gundo'
+Plugin 'octave.vim--'
+Plugin 'vcscommand.vim'
+
+"Clojure
+"Plugin 'tpope/vim-leiningen'
+"Plugin 'tpope/vim-classpath'
+Plugin 'tpope/vim-fireplace'
+Plugin 'tpope/vim-dispatch'
+Plugin 'guns/vim-clojure-static'
+Plugin 'guns/vim-clojure-highlight'
+Plugin 'paredit.vim'
+
+Plugin 'Shougo/vimproc'
+Plugin 'Shougo/neocomplete'
+Plugin 'Shougo/unite.vim'
+Plugin 'bling/vim-airline'
+Plugin 'bling/vim-bufferline'
+Plugin 'int3/vim-taglist-plus'
+Plugin 'scrooloose/syntastic'
+Plugin 'scrooloose/nerdtree'
+Plugin 'mhinz/vim-startify.git'
+Plugin 'Raimondi/delimitMate'
+Plugin 'ntpeters/vim-better-whitespace'
+Plugin 'freitass/todo.txt-vim'
+Plugin 'kshenoy/vim-signature'
+Plugin 'tpope/vim-repeat'
+Plugin 'tpope/vim-surround'
+
+Plugin 'derekwyatt/vim-scala'
+"Plugin 'ensime/ensime-vim'
+Plugin 'dscleaver/sbt-quickfix'
+Plugin 'solarnz/thrift.vim'
+
+Plugin 'pangloss/vim-javascript'
+Plugin 'leafgarland/typescript-vim'
+
+"cemetery
+"Plugin 'mileszs/ack.vim'
+"Plugin 'jimenezrick/vimerl'
+"Plugin 'Shougo/neosnippet'
+"Plugin 'kien/ctrlp.vim'
+"Plugin 'Lokaltog/vim-easymotion'
+"Plugin 'neilagabriel/vim-geeknote'
+"Plugin 'YankRing.vim'
+"Plugin 'ReplaceWithRegister'
+"Plugin 'perl-support.vim'
+
+"coloschemes
+Plugin 'larssmit/vim-getafe'
+if has('nvim')
+Plugin 'frankier/neovim-colors-solarized-truecolor-only'
+else
+Plugin 'altercation/vim-colors-solarized'
+endif
+Plugin 'tomasr/molokai'
+Plugin 'jonathanfilip/vim-lucius'
+Plugin 'djjcast/mirodark'
+Plugin 'bcat/abbott.vim'
+Plugin 'sjl/badwolf'
+Plugin 'baycomb'
+Plugin 'morhetz/gruvbox'
+
+
+call vundle#end()
+
+filetype plugin indent on
+syntax on
+
+"------------------------------------------------------------------------
+"  syntastic
+"------------------------------------------------------------------------
+
+"let g:syntastic_perl_checkers = ['perl', 'perlcritic', 'podchecker']
+let g:syntastic_perl_checkers = ['perl', 'podchecker']
+let g:syntastic_enable_perl_checker = 1
+
+let g:syntastic_scala_checkers = ['fsc']
+
+"------------------------------------------------------------------------
+"  clojure
+"------------------------------------------------------------------------
+
+let g:paredit_mode = 1
+
+"------------------------------------------------------------------------
+"  delimitMate
+"------------------------------------------------------------------------
+
+au FileType clojure let b:loaded_delimitMate = 1
+let delimitMate_expand_cr = 1
+
+"------------------------------------------------------------------------
+"  NERDTree
+"------------------------------------------------------------------------
+
+map <tab> :NERDTreeToggle<CR>
+
+"------------------------------------------------------------------------
+"  Unite
+"------------------------------------------------------------------------
+call unite#filters#matcher_default#use(['matcher_fuzzy'])
+
+let g:unite_data_directory='~/.unite_cache'
+"let g:unite_enable_start_insert=1
+let g:unite_source_history_yank_enable=1
+
+if executable('ag')
+    let g:unite_source_grep_command='ag'
+    let g:unite_source_grep_default_opts='--nocolor --nogroup -S -C4'
+    let g:unite_source_grep_recursive_opt=''
+elseif executable('ack')
+    let g:unite_source_grep_command='ack'
+    let g:unite_source_grep_default_opts='--no-heading --no-color -k'
+    let g:unite_source_grep_recursive_opt=''
+endif
+
+"nmap <space> [unite]
+nmap , [unite]
+nnoremap [unite] <nop>
+
+nnoremap <silent> <C-t> :<C-u>Unite -toggle -auto-resize -buffer-name=files -start-insert file_rec/async<cr>
+nnoremap <silent> [unite]y :<C-u>Unite -buffer-name=yanks history/yank<cr>
+nnoremap <silent> [unite]l :<C-u>Unite -auto-resize -buffer-name=line -start-insert line<cr>
+nnoremap <silent> [unite]b :<C-u>Unite -auto-resize -buffer-name=buffers -quick-match buffer<cr>
+"nnoremap <silent> [unite]m :<C-u>Unite -auto-resize -buffer-name=buffers -quick-match file_mru<cr>
+nnoremap <silent> [unite]/ :<C-u>Unite -no-quit -buffer-name=search grep:.<cr>
+
+"------------------------------------------------------------------------
+"  Neocomplete
+"------------------------------------------------------------------------
+let g:neocomplete#enable_at_startup = 1
+
+let g:neocomplete#force_overwrite_completefunc = 1
+
+let g:neocomplete#sources#syntax#min_keyword_length = 3
+
+let g:EclimCompletionMethod = "omnifunc"
+
+if !exists('g:neocomplete#force_omni_input_patterns')
+  let g:neocomplete#force_omni_input_patterns = {}
+endif
+let g:neocomplete#force_omni_input_patterns.java = '\k\.\k*'
+let g:neocomplete#force_omni_input_patterns.scala = '\k\.\k*'
+
+"------------------------------------------------------------------------
+"  Colors & Appearance
+"------------------------------------------------------------------------
+
+set background=dark
+"let g:lucius_contrast="high"
+
+"256 цветов
+set t_Co=256
+
+let $NVIM_TUI_ENABLE_TRUE_COLOR=1
+"colorscheme lucius
+"colorscheme solarized
+let g:molokai_original = 1
+colorscheme molokai
+
+"курсор не мигает
+let &guicursor = &guicursor . ",a:blinkon0"
+
+"для статусной строки
+set ttimeoutlen=50
+
+"remove scrollbar from gvim
+set guioptions+=RLlrb
+set guioptions-=RLlrb
+
+" Remove menu bar
+set guioptions-=m
+
+" Remove toolbar
+set guioptions-=T
+
+"------------------------------------------------------------------------
+" Autocmd
+"------------------------------------------------------------------------
+"при открытии файла прыгает к последнему месту
+au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$")
+    \| exe "normal! g'\"" | endif
+
+"расширения -> типы файлов
+au BufNewFile,BufRead *.exp         setfiletype tcl
+au BufNewFile,BufRead Make*         setfiletype make
+au BufNewFile,BufRead *.ocaml       setfiletype ocaml
+au BufNewFile,BufRead *.octave,*.m  setfiletype octave
+au BufNewFile,BufRead *.txt         setfiletype wiki
+
+au FileType c set omnifunc=ccomplete#Complete
+au FileType html set omnifunc=htmlcomplete#CompleteTags
+au FileType javascript set omnifunc=javascriptcomplete#CompleteJS
+
+"------------------------------------------------------------------------
+"  Maps
+"------------------------------------------------------------------------
+
+"выход по q
+noremap Q q
+noremap q :q<CR>
+
+cmap ц w
+cmap й q
+
+if bufwinnr(1)
+  map + <C-w>+
+  map - <C-w>-
+endif
+
+imap <C-d> <DEL>
+
+"Screen movement
+map j gj
+map k gk
+
+nnoremap Y y$
+
+"repeat last command for each line in visual block (Tip#84)
+vnoremap . :normal .<CR>
+
+"make <c-l> clear the highlight as well as redraw
+nnoremap <silent> <Esc><Esc> :nohlsearch<CR><Esc>
+
+inoremap <C-J> <esc>
+
+"-----------------------------------------------------------------------
+"  F-keys
+"------------------------------------------------------------------------
+
+"<F1> - помощь
+"<F2> - регистры
+"<F5> - Gundo
+"<F6> - открыть ссылку в браузере
+"<F7> - проверка орфографии
+"<F8> - кодировка
+"<F9> - компиляция/интерпретация
+
+map  <F1> :he <C-r><C-w><CR>
+map  <F2> :reg<CR>
+
+map  <F5> :GundoToggle<CR>
+map  <F6> :!urlview %<cr>
+map  <F7> :call ChangeSpellLang()<CR>
+
+set wildmenu
+set wcm=<Tab>
+menu Encoding.utf-8 :e ++enc=utf8 <CR>
+menu Encoding.windows-1251 :e ++enc=cp1251<CR>
+menu Encoding.koi8-r :e ++enc=koi8-r<CR>
+menu Encoding.cp866 :e ++enc=cp866<CR>
+map <F8> :emenu Encoding.<TAB>
+
+"-----------------------------------------------------------------------
+"  Leader
+"------------------------------------------------------------------------
+"
+nmap ,1  :b1<cr>
+nmap ,2  :b2<cr>
+nmap ,3  :b3<cr>
+nmap ,4  :b4<cr>
+nmap ,5  :b5<cr>
+nmap ,6  :b6<cr>
+nmap ,7  :b7<cr>
+nmap ,8  :b8<cr>
+nmap ,9  :b9<cr>
+nmap ,n  :bn<cr>
+nmap ,p  :bp<cr>
+
+set pastetoggle=<leader>p
+
+"-----------------------------------------------------------------------
+"  Launchers
+"------------------------------------------------------------------------
+
+au FileType c map <leader>rr :!gcc -lm % && ./a.out<CR>
+au FileType octave map <leader>rr :!octave -q %<CR>
+au FileType octave vmap <leader>rr y:!octave -q --eval "<c-r>0"<CR>
+
+au FileType scheme map <leader>rr :!guile %<CR>
+
+au FileType tex map <leader>rr :!pdflatex %<CR>
+au FileType tex map <leader>rs :!okular %<.pdf<CR>
+
+"au FileType perl map <leader>rr :!scp % root@172.16.4.60:~; ssh root@172.16.4.60 perl %<CR>
+au FileType perl map <leader>rr :!perl %<CR>
+
+"-----------------------------------------------------------------------
+"  sbt-quickfix
+"------------------------------------------------------------------------
+
+au FileType scala set errorformat=%E\ %#[error]\ %#%f:%l:\ %m,%-Z\ %#[error]\ %p^,%-C\ %#[error]\ %m
+au FileType scala set errorformat+=,%W\ %#[warn]\ %#%f:%l:\ %m,%-Z\ %#[warn]\ %p^,%-C\ %#[warn]\ %m
+au FileType scala set errorformat+=,%-G%.%#
+
+"noremap <silent> <Leader>ff :cf target/quickfix/sbt.quickfix<CR>
+"noremap <silent> <Leader>fn :cn<CR>
