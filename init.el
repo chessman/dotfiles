a2db495b57acd6aa3704f2769a1c4fb0317109b2
Eugene Apollonsky <e.apollonsky@gmail.com>
2019-01-07 23:14:57 +0200

    fix conflict

--- init.el
+++ init.el
@@ -11,11 +11,9 @@
                     ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 
-; FIXME: is it still needed?
-(when (version< emacs-version "26")
-    (package-initialize))
 (when (not package-archive-contents)
   (package-refresh-contents)
+  (package-initialize)
   (package-install 'use-package))
 
 (eval-when-compile
@@ -375,18 +373,11 @@ It looks for archive files in /pkg/."
 
 (use-package go-rename)
 
-<<<<<<< Updated upstream
 (use-package company-go
   :config
   (setq company-go-show-annotation t)
   (setq company-go-gocode-args '("-unimported-packages" "-builtin"))
   (add-to-list 'company-backends 'company-go))
-=======
-;; (use-package company-go
-;;   :config
-;;   (setq company-go-show-annotation t)
-;;   (add-to-list 'company-backends 'company-go))
->>>>>>> Stashed changes
 
 ;; (use-package company-tabnine
 ;;   :config
@@ -754,11 +745,8 @@ It looks for archive files in /pkg/."
  '(custom-safe-themes
    '("b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "31b2145c933e41fbbda48b15278cdcce3779db7e92ca434ad3044b3392ad6ae3" default))
  '(package-selected-packages
-<<<<<<< Updated upstream
    '(forge aggressive-indent company-tabnine flycheck-golangci-lint company-lsp lsp-go lsp-mode flycheck-gometalinter go-rename go-eldoc emacs-go-eldoc magithub yasnippet-snippets go-fill-struct build-status badwolf-theme go-impl godoctor yaml-mode avy go-add-tags indium company-go go-mode json-mode web-mode use-package tide smart-mode-line restclient php-mode org-pomodoro org-evil multitran monokai-theme molokai-theme js2-refactor helm-swoop helm-projectile helm-ag git-timemachine evil-smartparens evil-magit evil-leader ensime company-tern go-guru go-direx go-stacktracer dockerfile-mode auto-yasnippet protobuf-mode org-plus-contrib diminish)))
-=======
-   '(lsp-ui aggressive-indent company-tabnine flycheck-golangci-lint company-lsp lsp-go lsp-mode flycheck-gometalinter go-rename go-eldoc emacs-go-eldoc magithub yasnippet-snippets go-fill-struct build-status badwolf-theme go-impl godoctor yaml-mode avy go-add-tags indium company-go go-mode json-mode web-mode use-package tide smart-mode-line restclient php-mode org-pomodoro org-evil multitran monokai-theme molokai-theme js2-refactor helm-swoop helm-projectile helm-ag git-timemachine evil-smartparens evil-magit evil-leader ensime company-tern go-guru go-direx go-stacktracer dockerfile-mode auto-yasnippet protobuf-mode org-plus-contrib diminish)))
->>>>>>> Stashed changes
+))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
