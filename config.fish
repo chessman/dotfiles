936db77f3078bbbb9f1b25c14591eb1e5bad7dad
Eugene Apollonsky <e.apollonsky@gmail.com>
2019-04-05 00:26:13 +0300

    add local node and golang path setup

--- config.fish
+++ config.fish
@@ -1,2 +1,8 @@
 set fish_greeting
 set -gx PATH ~/bin $PATH
+set -gx PATH ~/gocode/bin $PATH
+set -gx PATH ~/node/bin $PATH
+set -gx NODE_PATH ~/node/lib/node_modules $NODE_PATH
+set -gx MANPATH ~/node/share/man $MANPATH
+
+
