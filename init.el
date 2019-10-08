207603cd916e2dd94b5a34f36ac75bb79ea2c3df
Eugene Apollonsky <e.apollonsky@gmail.com>
2019-10-08 23:48:55 +0300

    cleanup

--- init.el
+++ init.el
@@ -201,11 +201,9 @@
   :diminish projectile-mode
   :config
   (projectile-mode)
-  (add-to-list 'projectile-globally-ignored-directories ".ensime_cache")
   (add-to-list 'projectile-globally-ignored-directories ".bloop")
   (add-to-list 'projectile-globally-ignored-directories ".metals")
-  (add-to-list 'projectile-globally-ignored-directories "ltximg")
-  (add-to-list 'projectile-globally-ignored-files ".ensime"))
+  (add-to-list 'projectile-globally-ignored-directories "ltximg"))
 
 (use-package helm-projectile
  :config (evil-leader/set-key
@@ -214,8 +212,7 @@
 
 (use-package recentf
   :config
-  (recentf-mode t)
-  (add-to-list 'recentf-exclude "\\.ensime"))
+  (recentf-mode t))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Treemacs
@@ -770,9 +767,11 @@ It looks for archive files in /pkg/."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "31b2145c933e41fbbda48b15278cdcce3779db7e92ca434ad3044b3392ad6ae3" default))
+   (quote
+    ("11e57648ab04915568e558b77541d0e94e69d09c9c54c06075938b6abc0189d8" "174502267725776b47bdd2d220f035cae2c00c818765b138fea376b2cdc15eb6" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "31b2145c933e41fbbda48b15278cdcce3779db7e92ca434ad3044b3392ad6ae3" default)))
  '(package-selected-packages
-   '(org-jira evil-collection lsp-treemacs helm-lsp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs elm-mode forge aggressive-indent company-tabnine flycheck-golangci-lint company-lsp lsp-go lsp-mode flycheck-gometalinter go-rename go-eldoc emacs-go-eldoc magithub yasnippet-snippets go-fill-struct build-status badwolf-theme go-impl godoctor yaml-mode avy go-add-tags indium company-go go-mode json-mode web-mode use-package tide smart-mode-line restclient php-mode org-pomodoro org-evil multitran monokai-theme molokai-theme js2-refactor helm-swoop helm-projectile helm-ag git-timemachine evil-smartparens evil-magit evil-leader ensime company-tern go-guru go-direx go-stacktracer dockerfile-mode auto-yasnippet protobuf-mode org-plus-contrib diminish)))
+   (quote
+    (treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs elm-mode forge aggressive-indent company-tabnine flycheck-golangci-lint company-lsp lsp-go lsp-mode flycheck-gometalinter go-rename go-eldoc emacs-go-eldoc magithub yasnippet-snippets go-fill-struct build-status badwolf-theme go-impl godoctor yaml-mode avy go-add-tags indium company-go go-mode json-mode web-mode use-package smart-mode-line restclient php-mode org-pomodoro org-evil multitran monokai-theme molokai-theme js2-refactor helm-swoop helm-projectile helm-ag git-timemachine evil-smartparens evil-magit evil-leader company-tern go-guru go-direx go-stacktracer dockerfile-mode auto-yasnippet protobuf-mode org-plus-contrib diminish))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
