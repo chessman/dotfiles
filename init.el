3d3c72ed5eab77e2778ef185b55bc9b192b311fa
Eugene Apollonsky <ea@carbon>
2019-10-10 10:42:12 +0300

    move custom variables to another file :fireworks:

--- init.el
+++ init.el
@@ -22,6 +22,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Striping & Defaults
+(setq custom-file "~/.emacs.d/custom.el")
+
 (setq inhibit-startup-screen t)
 
 (setq frame-title-format '("" "%b @ Emacs " emacs-version))
@@ -754,21 +756,6 @@ It looks for archive files in /pkg/."
   (setq request-log-level 'debug)
   (setq request-message-level 'debug)
   (setq jiralib-url "https://intertrusttechnologies.atlassian.net"))
+
 ;; guaranteed kill
 (diminish 'auto-revert-mode)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("11e57648ab04915568e558b77541d0e94e69d09c9c54c06075938b6abc0189d8" "174502267725776b47bdd2d220f035cae2c00c818765b138fea376b2cdc15eb6" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "31b2145c933e41fbbda48b15278cdcce3779db7e92ca434ad3044b3392ad6ae3" default))
- '(package-selected-packages
-   '(flycheck-pos-tip treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs elm-mode forge aggressive-indent company-tabnine flycheck-golangci-lint company-lsp lsp-go lsp-mode flycheck-gometalinter go-rename go-eldoc emacs-go-eldoc magithub yasnippet-snippets go-fill-struct build-status badwolf-theme go-impl godoctor yaml-mode avy go-add-tags indium company-go go-mode json-mode web-mode use-package smart-mode-line restclient php-mode org-pomodoro org-evil multitran monokai-theme molokai-theme js2-refactor helm-swoop helm-projectile helm-ag git-timemachine evil-smartparens evil-magit evil-leader company-tern go-guru go-direx go-stacktracer dockerfile-mode auto-yasnippet protobuf-mode org-plus-contrib diminish)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
