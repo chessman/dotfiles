3a0a8026dbef034cdb9ce8fb09530424d8292b3c
Eugene Apollonsky <e.apollonsky@gmail.com>
2019-11-28 12:20:20 +0200

    projectile: remove bloop registration (merged upstream)

--- init.el
+++ init.el
@@ -195,13 +195,6 @@
   :config
   (projectile-mode)
 
-  (projectile-register-project-type 'bloop '(".bloop")
-                                    :compile "bloop compile root"
-                                    :test "bloop test --propagate --reporter scalac root"
-                                    :src-dir "src/main/"
-                                    :test-dir "src/test/"
-                                    :test-suffix "Spec")
-
   (setq projectile-create-missing-test-files t)
 
   (add-to-list 'projectile-globally-ignored-directories ".bloop")
