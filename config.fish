64aa13cffe244e03611e2d53d0577ef0e4b0fd8e
Eugene Apollonsky <e.apollonsky@gmail.com>
2020-05-26 14:41:34 +0300

    add coursier, remove gocode

--- config.fish
+++ config.fish
@@ -1,9 +1,7 @@
 set fish_greeting
 set -gx PATH ~/bin $PATH
-set -gx PATH ~/gocode/bin $PATH
 set -gx PATH ~/node/bin $PATH
+set -gx PATH ~/.local/share/coursier/bin $PATH
 set -gx NODE_PATH ~/node/lib/node_modules $NODE_PATH
-set -gx GOPATH ~/gocode
-
 
 starship init fish | source
