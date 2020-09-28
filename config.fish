5de64aba7d266c6a78bf3c4d0bf309c8253e7d45
Eugene Apollonsky <e.apollonsky@gmail.com>
2020-09-28 17:00:57 +0300

    more PATHs

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ set fish_greeting
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/node/bin $PATH
 set -gx PATH ~/.local/share/coursier/bin $PATH
+set -gx PATH ~/.gem/ruby/2.7.0/bin $PATH
+set -gx PATH ~/.local/bin $PATH
 set -gx NODE_PATH ~/node/lib/node_modules $NODE_PATH
 
 starship init fish | source
